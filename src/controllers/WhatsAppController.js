6bc749f07fdd23afe29f745e8902647ce05e7528
Thiago-l <thigo.l17@yahoo.com>
2020-10-10 18:14:06 -0300

    adding events

--- src/controllers/WhatsAppController.js
+++ src/controllers/WhatsAppController.js
@@ -69,6 +69,21 @@ class WhatsAppController{
             return this.classList.contains(name);
         }
 
+        HTMLFormElement.prototype.getForm = function() {
+
+            return new FormData(this);
+        }
+
+        HTMLFormElement.prototype.toJSON = function() {
+            let json = {};
+
+            this.getForm().forEach((value, key) => {
+
+                json[key] = value;
+            });
+            
+            return json;
+        }
     }
 
     initEvents() {
@@ -77,9 +92,9 @@ class WhatsAppController{
 
             this.closeAllLeftPanel();
             this.el.panelEditProfile.show();
+
             setTimeout(() => {
                 this.el.panelEditProfile.addClass('open');
-
             }, 300);
 
         });
@@ -88,9 +103,9 @@ class WhatsAppController{
             
             this.closeAllLeftPanel();
             this.el.panelAddContact.show();
+
             setTimeout(() => {
                 this.el.panelAddContact.addClass('open');
-
             }, 300);
             
         });
@@ -104,6 +119,36 @@ class WhatsAppController{
         this.el.btnClosePanelAddContact.on('click', e => {
 
             this.el.panelAddContact.removeClass('open');
+
+        });
+
+        this.el.photoContainerEditProfile.on('click', e => {
+
+            this.el.inputProfilePhoto.click();
+
+        });
+
+        this.el.inputNamePanelEditProfile.on('keypress', e => {
+
+            if (e.key === 'Enter') {
+
+                e.preventDefault();
+                this.el.btnSavePanelEditProfile.click();
+            }
+
+        });
+
+        this.el.btnSavePanelEditProfile.on('click', e => {
+
+            console.log(this.el.inputNamePanelEditProfile.innerHTML);
+
+        });
+
+        this.el.formPanelAddContact.on('submit', e => {
+
+            e.preventDefault();
+
+            let formData = new FormData(this.el.formPanelAddContact);
 
         });
     }
