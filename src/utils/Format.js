393a0089bd4748e30acc2e4ea5136611c7225999
Thiago-l <thigo.l17@yahoo.com>
2020-10-10 17:10:21 -0300

    adding controller and features

---
+++ src/utils/Format.js
@@ -0,0 +1,11 @@
+class Format {
+    
+    static getCamelCase(text) {
+
+        let div = document.createElement('div');
+
+        div.innerHTML = `<div data-${text}="id"></div>`;
+
+        return Object.keys(div.firstChild.dataset)[0];
+    }
+}
